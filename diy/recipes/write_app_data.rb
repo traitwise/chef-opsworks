8f3f0f74ae2c85b647ac15d74dc426dde261b500
thomas <blomcode@gmail.com>
2014-01-22 11:48:58 -0600

    debug comments for chef recipe

--- diy/recipes/write_app_data.rb
+++ diy/recipes/write_app_data.rb
@@ -1,9 +1,9 @@
 
-filename = File.join(deploy[:deploy_to], 'current/config', 'app_data.yml')
-
-Chef::Log.info("Writing app data to #{filename}")
+Chef::Log.info("Executing write_app_data.rb ... ")
 
 node[:deploy].each do |app, deploy|
+  filename = File.join(deploy[:deploy_to], 'current/config', 'app_data.yml')
+  Chef::Log.info(" --> app data to #{filename}")
   File.open(filename,'w') do |file|
     hash = node[:app_data][app].to_hash
     Chef::Log.info("Contents should be: #{hash.to_yaml}")    

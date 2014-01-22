84ea2993c34341c9da424143f796a050e0942282
thomas <blomcode@gmail.com>
2014-01-22 15:00:38 -0600

    config files'

--- diy/recipes/write_app_data.rb
+++ diy/recipes/write_app_data.rb
@@ -2,10 +2,11 @@
 Chef::Log.info("Executing write_app_data.rb ... ")
 
 node[:deploy].each do |app, deploy|
-  filename = File.join(deploy[:deploy_to], 'current/config', 'app_data.yml')
+  configPath = File.join( deploy[:deploy_to], 'current/config' )
+  filename = File.join( configPath, filename )
   Chef::Log.info(" --> app data to #{filename}")
-  actualFile = File.readlink( filename )
-  Chef::Log.info(" ( this is actually a symlink to #{actualFile} )")
+  actualPath = File.readlink( configPath )
+  Chef::Log.info(" ( this is actually a symlink to the path #{actualPath} )")
   File.open(filename,'w') do |file|
     hash = node[:app_data][app].to_hash
     Chef::Log.info("Contents should be: #{hash.to_yaml}")    

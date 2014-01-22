c9d2429f71e600386462d5c302d3e0c0c123e077
thomas <blomcode@gmail.com>
2014-01-22 17:12:02 -0600

    try writing to release_path

--- diy/recipes/write_app_data.rb
+++ diy/recipes/write_app_data.rb
@@ -2,11 +2,22 @@
 Chef::Log.info("Executing write_app_data.rb ... ")
 
 node[:deploy].each do |app, deploy|
-  filename = File.join( deploy[:deploy_to], 'current/config', 'app_data.yml' )
+  filename = File.join( deploy[:deploy_to], 'app_data.yml' )
   Chef::Log.info(" --> app data will be written to #{filename}")
   File.open(filename,'w') do |file|
     hash = node[:app_data][app].to_hash
     Chef::Log.info("Contents should be: #{hash.to_yaml}")    
     file.write YAML.dump( hash )
   end
+
+  Chef::Log.info("Now try writing it to release_path, which is #{release_path}")
+
+  filename = File.join( release_path, 'config', 'app_data.yml' )
+  Chef::Log.info(" --> app data will be written to #{filename}")
+  File.open(filename,'w') do |file|
+    hash = node[:app_data][app].to_hash
+    Chef::Log.info("Contents should be: #{hash.to_yaml}")    
+    file.write YAML.dump( hash )
+  end
+
 end

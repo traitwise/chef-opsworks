193937be5be5f8055ea87486221a2ee479cf52d8
thomas <blomcode@gmail.com>
2014-01-22 17:21:30 -0600

    try writing to parent of rails root

--- diy/recipes/write_app_data.rb
+++ diy/recipes/write_app_data.rb
@@ -1,18 +1,13 @@
 
 Chef::Log.info("Executing write_app_data.rb ... ")
 
+#
+# I want to this this file into current/config, but the current symlink has not been
+# updated yet to point to this new deploy which has just been placed on disk.  How do
+# I get the path to the config folder of the deploy in progress?
+#
 node[:deploy].each do |app, deploy|
   filename = File.join( deploy[:deploy_to], 'app_data.yml' )
-  Chef::Log.info(" --> app data will be written to #{filename}")
-  File.open(filename,'w') do |file|
-    hash = node[:app_data][app].to_hash
-    Chef::Log.info("Contents should be: #{hash.to_yaml}")    
-    file.write YAML.dump( hash )
-  end
-
-  Chef::Log.info("Now try writing it to release_path, which is #{release_path}")
-
-  filename = File.join( release_path, 'config', 'app_data.yml' )
   Chef::Log.info(" --> app data will be written to #{filename}")
   File.open(filename,'w') do |file|
     hash = node[:app_data][app].to_hash

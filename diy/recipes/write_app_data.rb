f1661ed215bc1528820696755da5892442c1805d
thomas <blomcode@gmail.com>
2014-01-23 09:20:23 -0600

    fix logic bug

--- diy/recipes/write_app_data.rb
+++ diy/recipes/write_app_data.rb
@@ -9,12 +9,14 @@ Chef::Log.info("Executing write_app_data.rb ... ")
 
 #Chef::Log.info( "The node object contains: #{node.to_hash.to_yaml}")
 
-dest = File.join( deploy[:deploy_to], 'shared', 'config' )
-Dir.mkdir( dest, 0755 )
 
 node[:deploy].each do |app, deploy|
+  dest = File.join( deploy[:deploy_to], 'shared', 'config' )
+  Dir.mkdir( dest, 0755 )
+
   filename = File.join( dest, 'app_data.yml' )
   Chef::Log.info(" --> app data will be written to #{filename}")
+  
   File.open(filename,'w') do |file|
     hash = node[:app_data][app].to_hash
     Chef::Log.info("Contents should be: #{hash.to_yaml}")    

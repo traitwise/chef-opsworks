d71cb3b0fe24d284da51c2e1f657afaa31894dd4
thomas <blomcode@gmail.com>
2014-01-24 18:21:05 -0600

    add delayed_job cookbook forked elsewhere

---
+++ delayed_job/metadata.rb
@@ -0,0 +1,13 @@
+maintainer       "Artsy"
+maintainer_email "it@artsymail.com"
+license          "MIT"
+description      "Configure and deploy background job workers."
+version          "0.3"
+
+recipe 'opsworks_delayed_job::setup', 'Set up delayed_job worker.'
+recipe 'opsworks_delayed_job::configure', 'Configure delayed_job worker.'
+recipe 'opsworks_delayed_job::deploy', 'Deploy delayed_job worker.'
+recipe 'opsworks_delayed_job::undeploy', 'Undeploy delayed_job worker.'
+recipe 'opsworks_delayed_job::stop', 'Stop delayed_job worker.'
+
+depends 'deploy'

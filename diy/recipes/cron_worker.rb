71d6c7fc1ece90771c40542094e85e837ef1de96
thomas <blomcode@gmail.com>
2014-01-30 12:26:01 -0600

    try chef cron with just one job

--- diy/recipes/cron_worker.rb
+++ diy/recipes/cron_worker.rb
@@ -20,43 +20,43 @@ end
 # Run full analytics once overnight.  Will get imported by above.
 # 3am
 #
-cron 'run_full_analytics' do
-    hour "3"
-    minute "0"
-    command '/srv/www/diy_prod/user_scripts/run_full_twstat_analytics.sh'
-end
+# cron 'run_full_analytics' do
+#     hour "3"
+#     minute "0"
+#     command '/srv/www/diy_prod/user_scripts/run_full_twstat_analytics.sh'
+# end
 
 
-#
-# Delete expired secure iframes 
-# 2am
-#
-cron 'db_housekeeping_iframes' do
-    hour "2"
-    minute "0"
-    command 'cd /srv/www/diy_prod/current; ./script/rails runner -e production /srv/www/diy_prod/current/lib/remove_old_secure_iframes.rb'
-end
+# #
+# # Delete expired secure iframes 
+# # 2am
+# #
+# cron 'db_housekeeping_iframes' do
+#     hour "2"
+#     minute "0"
+#     command 'cd /srv/www/diy_prod/current; ./script/rails runner -e production /srv/www/diy_prod/current/lib/remove_old_secure_iframes.rb'
+# end
 
-#
-# Every request to a TW other than specific diy_controller pages create anonymous users.  Remove users
-# that are "blank" and of a certain age.
-# 2:10am
-#
-cron 'db_housekeeping_users' do
-    hour "2"
-    minute "10"
-    command => 'cd /srv/www/diy_prod/current; ./script/rails runner -e production /srv/www/diy_prod/current/lib/remove_blank_users.rb'
-end
+# #
+# # Every request to a TW other than specific diy_controller pages create anonymous users.  Remove users
+# # that are "blank" and of a certain age.
+# # 2:10am
+# #
+# cron 'db_housekeeping_users' do
+#     hour "2"
+#     minute "10"
+#     command => 'cd /srv/www/diy_prod/current; ./script/rails runner -e production /srv/www/diy_prod/current/lib/remove_blank_users.rb'
+# end
 
-#
-# Housekeeping specific to the GA project. Cause database panels (conditions, medications, symptoms) to be asked
-# upon each visit if the user has reported none previously.  
-# 2:20am
-#
-cron 'ga_housekeeping_dbchoose' do
-    hour "2"
-    minute "20"
-    command 'cd /srv/www/diy_prod/current; ./script/rails runner -e production /srv/www/diy_prod/current/lib/clear_blank_dbchoose_responses.rb'
-end
+# #
+# # Housekeeping specific to the GA project. Cause database panels (conditions, medications, symptoms) to be asked
+# # upon each visit if the user has reported none previously.  
+# # 2:20am
+# #
+# cron 'ga_housekeeping_dbchoose' do
+#     hour "2"
+#     minute "20"
+#     command 'cd /srv/www/diy_prod/current; ./script/rails runner -e production /srv/www/diy_prod/current/lib/clear_blank_dbchoose_responses.rb'
+# end
 
 

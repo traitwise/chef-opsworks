b0cf4906f0cb6c98fcda2cb48fa4f9077adaf464
thomas <blomcode@gmail.com>
2014-02-10 17:34:15 -0600

    cron backup database mod

--- diy/recipes/cron_backup_database.rb
+++ diy/recipes/cron_backup_database.rb
@@ -15,6 +15,6 @@
 cron 'backup_database' do
     user "deploy"
     minute "2"
-    command 'cd /home/deploy && rake backup_database'
+    command 'cd /srv/www/diy_prod/current; rake backup_database'
 end
 

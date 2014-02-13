2c5622562072aa1ef96156becd0ac887bf5f1fed
thomas <blomcode@gmail.com>
2014-02-12 18:48:25 -0600

    cron test

--- diy/recipes/cron_full_analytics_skipq2q.rb
+++ diy/recipes/cron_full_analytics_skipq2q.rb
@@ -15,8 +15,8 @@
 #
 cron 'run_full_analytics' do
     user "deploy"
-    hour "3"
-    minute "0"
+    hour "0"
+    minute "50"
     command '/srv/www/diy_prod/current/user_scripts/run_full_twstat_analytics.sh --skipQ2Q'
 end
 

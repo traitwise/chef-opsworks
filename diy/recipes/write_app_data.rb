54154e84a9edc7f47894504301ed5517aafffaf8
thomas <blomcode@gmail.com>
2014-02-14 12:33:24 -0600

    remove debug info from write_app_data

--- diy/recipes/write_app_data.rb
+++ diy/recipes/write_app_data.rb
@@ -1,6 +1,7 @@
 
-me = `whoami`
+Chef::Log.info( "In write_app_data.rb...")
 
+me = `whoami`
 Chef::Log.info("Executing write_app_data.rb as #{me}... ")
 
 #
@@ -9,37 +10,12 @@ Chef::Log.info("Executing write_app_data.rb as #{me}... ")
 # I get the path to the config folder of the deploy in progress?
 #
 
-#Chef::Log.info( "The node object contains: #{node.to_hash.to_yaml}")
-
-Chef::Log.info( "*** WAD")
-Chef::Log.info( "node[:app_data] is #{node[:app_data].to_hash.to_yaml}" ) if node[:app_data]
-
-
 node[:deploy].each do |app, deploy|
-  Chef::Log.info( "*** WAD")
-  path = "#{deploy[:deploy_to]}/shared/config"
-  shared_exists = File.exist? path
-  Chef::Log.info( "The path #{path} exists -> #{shared_exists}")
-
-  Chef::Log.info( "*** WAD")
-  Chef::Log.info( "app is #{app.to_s}")
-  Chef::Log.info( "*** WAD")
-  Chef::Log.info( "deploy is #{deploy.to_hash.to_yaml}")
-
-
-
   if node[:app_data][app]
-    Chef::Log.info( "*** WAD")
     Chef::Log.info( "Writing to #{deploy[:deploy_to]}/shared/config/app_data.yml app_data: #{node[:app_data][app].to_hash.to_yaml}")
 
     file File.join(deploy[:deploy_to], 'shared', 'config', 'app_data.yml') do
       content YAML.dump(node[:app_data][app].to_hash)
     end
   end
-
-  Chef::Log.info( "*** WAD")
-  path = "#{deploy[:deploy_to]}/shared/config/app_data.yml"
-  shared_exists = File.exist? path
-  Chef::Log.info( "The path #{path} exists -> #{shared_exists}")
-
 end
